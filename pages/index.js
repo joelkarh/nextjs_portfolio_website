70d24ec75f9fbc8d758fcc3b5ced1227477a5e23
Joël Karhambae <joelkarhamba@hotmail.com>
2021-09-24 05:25:07 +0200

    Update ignored files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import Layout from '../src/layouts/Layout'
+import Layout from '../src/components/layout'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
